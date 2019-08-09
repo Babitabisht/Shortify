a8b611b564a73bd61980b2462522ad54a81b4454
babitabisht <babitabisht2018@gmail.com>
2019-08-09 22:19:29 +0530

    make it better6

--- Frontend/shortifyFrontend/src/app/main/main.component.ts
+++ Frontend/shortifyFrontend/src/app/main/main.component.ts
@@ -19,17 +19,17 @@ export class MainComponent implements OnInit {
     this.shortURL = '';
     this.err = '';
     if (this.val == null || this.val == undefined) {
-      console.log('add something');
+    //  console.log('add something');
     } else {
-      console.log('in else');
+     // console.log('in else');
 
       this.shortify.shortifytheURL(this.val).subscribe((data: any) => {
-        console.log(
-          '-------------This is the response---',
-          data,
-          typeof data,
-          data.shortURL
-        );
+        // console.log(
+        //   '-------------This is the response---',
+        //   data,
+        //   typeof data,
+        //   data.shortURL
+        // );
         if (data == 422) {
           this.err = 'Incorrect URL';
         }
